8519e87359b742775d131bf48772ee7142d1ac3f
hadi mohammadi <work.hadimohammadi@gmail.com>
2023-03-04 02:46:58 +0330

    add build src

---
+++ buildSrc/src/main/kotlin/AndroidX.kt
@@ -0,0 +1,16 @@
+object AndroidX {
+    const val composeVersion = "1.3.3"
+    private const val navigationVersion = "2.4.2"
+
+    const val coreKtx = "androidx.core:core-ktx:1.9.0"
+    const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1"
+    const val activityCompose = "androidx.activity:activity-compose:1.6.1"
+    const val composeUi = "androidx.compose.ui:ui:$composeVersion"
+    const val composeUiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$composeVersion"
+    const val composeMaterial = "androidx.compose.material:material:1.3.1"
+    const val testJUnit = "androidx.test.ext:junit:1.1.5"
+    const val testEspresso = "androidx.test.espresso:espresso-core:3.5.1"
+    const val composeUiTestJUnit = "androidx.compose.ui:ui-test-junit4:$composeVersion"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling:$composeVersion"
+    const val composeTestManifest = "androidx.compose.ui:ui-test-manifest:$composeVersion"
+}
---
+++ buildSrc/src/main/kotlin/JUnit.kt
@@ -0,0 +1,4 @@
+object JUnit {
+    private const val jUnitVersion = "4.13.2"
+    const val junit = "junit:junit:$jUnitVersion"
+}
